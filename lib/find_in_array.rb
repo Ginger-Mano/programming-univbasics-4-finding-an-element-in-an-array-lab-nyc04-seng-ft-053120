0c9c536121fc583aa034b60bb7bd19353b9991b9
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-06 20:11:46 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,6 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  index = 0
+  while array.index(value_to_find) do
+    puts array.index
+  end# Add your solution here
 end
